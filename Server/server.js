7e736546ed43bc2ef07b3f86681e75a51aa23457
AleDavMart <aleyda.davalos@gmail.com>
2022-01-24 20:50:30 -0800

    added express handle bars to assist with server

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,33 @@
 //imports
-require('dotenv').config()
+// require('dotenv').config()
+const dotenv = require("dotenv"); //why is this better?
 const express = require ('express');
-const app = express();
+const hbs = require ('express-handlebars'); 
 const PORT = process.env.PORT || 3001; //if no env variable is set then default to 3001
 const cors = require ('cors');
+const path = require ('path');
+const morgan = require ('morgan');
+const {uuid} = require ('uuidv4');
+const app = express();
+
+//setting up morgan for request logging
+app.use(morgan("dev"));
+
+//Parsing JSON bodies
+app.use(exppress.json());
+
+//Parsing URL encoded bodies 
+app.use(express.urlencoded({extended: true})); 
+
+//Serve client from build folder
+app.use(express.static(path.join(__dirname, "/client")));
+
+//Enabling environment variables by parsing the .env file
+dotenv.config({
+    path: './.env'
+});
+
+
 
 //middleware 
 app.use(cors());
